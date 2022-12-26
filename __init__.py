4dd65a56e3d7afb90934948809b98a8d184e4a98
Saagar Jha <saagar@saagarjha.com>
2022-12-26 07:30:23 -0800

    Demangle variables in addition to functions

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 import binaryninja
 import subprocess
 
+def run_demanger(symbols):
+	return subprocess.check_output(["swift", "demangle", "--compact", "--simplified"], input="\n".join(symbols).encode()).decode().strip().split("\n")
+
 def demangle_swift(bv):
 	swift_functions = []
 	for function in bv.functions:
 		if function.name.startswith("_$s"):
 			swift_functions.append(function)
-	results = subprocess.check_output(["xcrun", "swift-demangle", "--compact", "--simplified"], input="\n".join(map(lambda f: f.name, swift_functions)).encode()).decode().strip().split("\n")
+	results = run_demanger(map(lambda f: f.name, swift_functions))
 	assert len(swift_functions) == len(results)
 	for (function, name) in zip(swift_functions, results):
 		if function.comment:
@@ -14,5 +17,19 @@ def demangle_swift(bv):
 		else:
 			function.comment = function.name
 		function.name = name
+
+	variables = bv.data_vars
+	swift_variables = []
+	for variable in variables:
+		try:
+			variable = variables[variable]
+			if variable.name.startswith("_$s"):
+				swift_variables.append(variable)
+		except:
+			pass
+	results = run_demanger(map(lambda v: v.name, swift_variables))
+	assert len(swift_variables) == len(results)
+	for (variable, name) in zip(swift_variables, results):
+		variable.name = name
 
 binaryninja.PluginCommand.register("Swift Demangler", "Demangles Swift", demangle_swift)
